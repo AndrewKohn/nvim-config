1dacd910502b57829dd2aa45219facc0ea634d09
Andrew Kohn <contact@kohnandrew.com>
2023-09-04 20:29:46 -0500

    Adjust: separated plugins w/ comments

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,11 +5,11 @@
   -- visual_block_mode = "x",
   -- term_mode = "t",
   -- command_mode = "c",
-------------------------------------
+-----------------------------------------------------------
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 
-
+-----------------------------------------------------------
 -- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
@@ -18,7 +18,7 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
-
+-----------------------------------------------------------
 -- harpoon
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
@@ -30,15 +30,15 @@ vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
 
-
+-----------------------------------------------------------
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
-
+-----------------------------------------------------------
 -- vim-fugitive
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-
+-----------------------------------------------------------
 -- neoscroll
 require('neoscroll').setup({
     easing_function = "quadratic",
@@ -51,6 +51,8 @@ t['<C-]>'] = {'scroll', { 'vim.wo.scroll', 'true', '250'}}
 
 require('neoscroll.config').set_mappings(t)
 
-
+-----------------------------------------------------------
 -- zen-mode
 vim.keymap.set("n", "<leader>zm", vim.cmd.ZenMode)
+
+-----------------------------------------------------------
