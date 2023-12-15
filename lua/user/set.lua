97e48c125ad7b0340464499dd55beaa72b291daa
andrewkohn <contact@kohnandrew.com>
2023-12-15 08:25:41 -0600

    Update: set tabstops/colorcolumn sizes

--- lua/user/set.lua
+++ lua/user/set.lua
@@ -5,11 +5,12 @@ vim.opt.incsearch = true
 -- Make line numbers default
 vim.wo.number = true
 vim.opt.relativenumber = true
+vim.opt.colorcolumn = "80"
 
 -- indentations
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
