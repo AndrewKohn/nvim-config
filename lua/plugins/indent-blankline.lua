bbcfebc6b4dba930638b16c113e4419120210a29
Andrew Kohn <contact@kohnandrew.com>
2024-02-05 22:45:56 -0600

    Update: plugin configs

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,15 +1,50 @@
 -- indent-blankline
 -- displays indents & spaces for code readability.
--- TODO: go thru docs and migrate v2 to v3...
 
 return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		main = "ibl",
-		opts = {
-			indent = {
-				char = "┊",
-			},
-		},
+		config = function()
+			local ibl = require("ibl")
+			local hooks = require("ibl.hooks")
+
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "fg_gray1", { fg = "#393836" })
+				vim.api.nvim_set_hl(0, "fg_gray2", { fg = "#4c494a" })
+				vim.api.nvim_set_hl(0, "bg_black1", { bg = "#181616" })
+				vim.api.nvim_set_hl(0, "bg_black2", { bg = "#1F1D1D" })
+				--vim.api.nvim_set_hl(0, "fg_lightyellow", { fg = "#f1f0cc" })
+				vim.api.nvim_set_hl(0, "focus", { fg = "#80a0ff" })
+			end)
+
+			local fg_highlights = {
+				"fg_gray1",
+				"fg_gray2",
+			}
+
+			local bg_highlights = {
+				"bg_black1",
+				"bg_black2",
+			}
+
+			local opts = {
+				indent = {
+					char = "┊",
+					highlight = fg_highlights,
+				},
+				scope = {
+					enabled = true,
+					highlight = "focus",
+				},
+				whitespace = {
+					highlight = bg_highlights,
+					remove_blankline_trail = false,
+				},
+			}
+
+			ibl.setup(opts)
+		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,33 @@
 -- telescope
 -- the fuzziest of finders
--- TODO: Add ui-select for code actions.  current pr is handling ca issues...
+-- TODO:
+-- Add ui-select for code actions.  current pr is handling ca issues...
+-- configure telescope layout.  Vertical?
 
 return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.3",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
+		opts = {
+			defaults = {
+				prompt_prefix = " ",
+				selection_caret = " ",
+				path_display = { "smart" },
+				sorting_strategy = "ascending",
+				layout_config = {
+					prompt_position = "top",
+					horizontal = {
+						preview_cutoff = 80,
+						width = 0.9,
+					},
+				},
+			},
+		},
+		config = function(_, opts)
 			local telescope = require("telescope")
 
+			telescope.setup(opts)
 			telescope.load_extension("harpoon")
 		end,
 	},
