d38ff532a10d0e675c9086160e63d4dabc7843e2
Andrew Kohn <contact@kohnandrew.com>
2024-02-15 19:56:26 -0600

    Update: added more languages to plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,10 +6,26 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			-- Conform will run multiple formatters sequentially
-			python = { "isort", "black" },
-			-- Use a sub-list to run only the first available formatter
+			svelte = { { "prettierd", "prettier" } },
 			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			javascriptreact = { { "prettierd", "prettier" } },
+			typescriptreact = { { "prettierd", "prettier" } },
+			json = { { "prettierd", "prettier" } },
+			graphql = { { "prettierd", "prettier" } },
+			java = { "google-java-format" },
+			kotlin = { "ktlint" },
+			ruby = { "standardrb" },
+			markdown = { { "prettierd", "prettier" } },
+			erb = { "htmlbeautifier" },
+			html = { "htmlbeautifier" },
+			bash = { "beautysh" },
+			proto = { "buf" },
+			rust = { "rustfmt" },
+			yaml = { "yamlfix" },
+			toml = { "taplo" },
+			css = { { "prettierd", "prettier" } },
+			scss = { { "prettierd", "prettier" } },
 		},
 		format_on_save = {
 			-- These options will be passed to conform.format()
