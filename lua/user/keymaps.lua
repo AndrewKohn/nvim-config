a77c62eb2829170eb3e139f7b8fb0c918f0eac50
andrewkohn <contact@kohnandrew.com>
2024-02-18 14:08:55 -0600

    Update: Add keymap & descriptions

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -9,16 +9,24 @@
 -- command_mode = "c",
 -----------------------------------------------------------
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle, { desc = "NvimTree: toggle tree" })
 vim.keymap.set("n", "Q", "<nop>")
 
 -- move selected text up/down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Vim: move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Vim: move selection up" })
 
 -- keeps search terms in middle of screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", { desc = "Vim: go to previous search" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Vim: go to next search" })
 
 -- Allows replacing the word (eg. variable) and changes all instances within the file
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set(
+	"n",
+	"<leader>s",
+	":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+	{ desc = "Vim: refactor variable" }
+)
+
+-- code actions
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Vim: code actions" })
--- lua/user/plugin_keymaps.lua
+++ lua/user/plugin_keymaps.lua
@@ -11,70 +11,70 @@ vim.g.loaded_netrwPlugin = 1
 -----------------------------------------------------------
 -- telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files)
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope: find files" })
+vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope: git files" })
 vim.keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>pb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>pm", ":Telescope harpoon marks<CR>", {})
-vim.keymap.set("n", "<leader>pn", ":Telescope noice<CR>", {})
+end, { desc = "Telescope: grep" })
+vim.keymap.set("n", "<leader>ph", builtin.help_tags, { desc = "Telescope: help tags" })
+vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "Telescope: buffers" })
+vim.keymap.set("n", "<leader>pm", ":Telescope harpoon marks<CR>", { desc = "Telescope: harpoon marks" })
+vim.keymap.set("n", "<leader>pn", ":Telescope noice<CR>", { desc = "Telescope: noice notifications" })
 
 -----------------------------------------------------------
 -- harpoon
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: add file" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon: toggle menu" })
 
 vim.keymap.set("n", "<a-1>", function()
 	ui.nav_file(1)
-end)
+end, { desc = "Harpoon: mark 1 jump" })
 vim.keymap.set("n", "<a-2>", function()
 	ui.nav_file(2)
-end)
+end, { desc = "Harpoon: mark 2 jump" })
 vim.keymap.set("n", "<a-3>", function()
 	ui.nav_file(3)
-end)
+end, { desc = "Harpoon: mark 3 jump" })
 vim.keymap.set("n", "<a-4>", function()
 	ui.nav_file(4)
-end)
+end, { desc = "Harpoon: mark 4 jump" })
 
 -----------------------------------------------------------
 -- trouble
 local trouble = require("trouble")
 vim.keymap.set("n", "<leader>tt", function()
 	trouble.toggle()
-end)
+end, { desc = "Trouble: toggle menu" })
 vim.keymap.set("n", "<leader>tw", function()
 	trouble.toggle("workspace_diagnostics")
-end)
+end, { desc = "Trouble: toggle workspace diagnostics" })
 vim.keymap.set("n", "<leader>td", function()
 	trouble.toggle("document_diagnostics")
-end)
+end, { desc = "Trouble: toggle document diagnostics" })
 vim.keymap.set("n", "<leader>tq", function()
 	trouble.toggle("quickfix")
-end)
+end, { desc = "Trouble: toggle quickfix" })
 vim.keymap.set("n", "<leader>tl", function()
 	trouble.toggle("loclist")
-end)
-vim.keymap.set("n", "gR", function()
+end, { desc = "Trouble: toggle loclist" })
+vim.keymap.set("n", "<leader>tr", function()
 	trouble.toggle("lsp_references")
-end)
+end, { desc = "Trouble: toggle lsp refs" })
 
 -----------------------------------------------------------
 -- zen-mode
-vim.keymap.set("n", "<leader>zm", vim.cmd.ZenMode)
+vim.keymap.set("n", "<leader>zm", vim.cmd.ZenMode, { desc = "Zen-mode: toggle" })
 
 -----------------------------------------------------------
 -- undotree
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Undotree: toggle sidebar" })
 
 -----------------------------------------------------------
 -- vim-fugitive
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>ds", vim.cmd.Gvdiffsplit)
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Fugitive: git" })
+vim.keymap.set("n", "<leader>ds", vim.cmd.Gvdiffsplit, { desc = "Fugitive: vertical split" })
 
 -----------------------------------------------------------
 -- neoscroll
@@ -92,28 +92,28 @@ require("neoscroll.config").set_mappings(t)
 -----------------------------------------------------------
 -- gitsigns
 vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {
-	desc = "Gitsigns preview hunk",
+	desc = "Gitsigns: preview hunk",
 })
 vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {
-	desc = "Gitsigns toggle blame current line",
+	desc = "Gitsigns: toggle blame current line",
 })
 
 -----------------------------------------------------------
 -- none-ls (null-ls)
 vim.keymap.set("n", "ff", vim.lsp.buf.format, {
-	desc = "format file",
+	desc = "Vim: format file",
 })
 
 -----------------------------------------------------------
 -- pretty_hover
 vim.keymap.set("n", "K", function()
 	require("pretty_hover").hover()
-end, { desc = "pretty_hover hover" })
+end, { desc = "pretty_hover: display hover info" })
 
 -----------------------------------------------------------
 -- comment
 -- NOTE: <C-_> = ctrl + /
-vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
-vim.api.nvim_set_keymap("v", "<C-_>", "gc", { noremap = false })
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false, desc = "Comment: comment line" })
+vim.api.nvim_set_keymap("v", "<C-_>", "gc", { noremap = false, desc = "Comment: comment selection" })
 
 -----------------------------------------------------------
