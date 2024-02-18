4e7fd25df691867ff9a9c521f84bb01781ca5583
andrewkohn <contact@kohnandrew.com>
2024-02-18 14:04:58 -0600

    Adjust: notification position

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,25 +5,30 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				max_width = 70,
+				render = "compact",
+				timeout = 1000, -- 1s after animation ends
+				top_down = false,
+			},
+		},
 	},
-	opts = function()
-		--FIX: set nvim hl groups and correct border & icon colors
-
-		return {
-			views = {
-				cmdline_popup = {
-					border = {
-						style = "rounded",
-						padding = { 1, 1 },
-					},
+	opts = {
+		views = {
+			cmdline_popup = {
+				border = {
+					style = "rounded",
+					padding = { 1, 1 },
+				},
+				size = {
+					min_width = 60,
+					width = "auto",
+					height = "auto",
 				},
 			},
-		}
-	end,
+		},
+	},
 }
