1ba1fa11be25fb9464e9de89dfc6f0a0e9837534
andrewkohn <contact@kohnandrew.com>
2024-03-01 05:38:03 -0600

    Update: formatter on_attach added

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,17 +2,35 @@
 -- community-maintained null-ls fork
 
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua, -- lua
-				null_ls.builtins.formatting.prettier, -- js/ts formatter
-				null_ls.builtins.formatting.black, -- python
-				--null_ls.builtins.completion.spell,
-			},
-		})
-	end,
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua, -- lua
+                null_ls.builtins.formatting.prettier, -- js/ts formatter
+                null_ls.builtins.formatting.black, -- python
+                --null_ls.builtins.completion.spell,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({
+                                bufnr = bufnr,
+                                filter = function()
+                                    return client.name == "null-ls"
+                                end,
+                            })
+                        end,
+                    })
+                end
+            end,
+        })
+    end,
 }
