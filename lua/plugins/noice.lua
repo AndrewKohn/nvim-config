905d19c027c326a96a79ccd476b49bb545b306ad
andrewkohn <contact@kohnandrew.com>
2024-04-05 15:19:04 -0500

    Update: notification timing changed

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,33 +2,33 @@
 -- nice ui additions
 
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		{
-			"rcarriga/nvim-notify",
-			opts = {
-				max_width = 70,
-				render = "compact",
-				timeout = 1000, -- 1s after animation ends
-				top_down = false,
-			},
-		},
-	},
-	opts = {
-		views = {
-			cmdline_popup = {
-				border = {
-					style = "rounded",
-					padding = { 1, 1 },
-				},
-				size = {
-					min_width = 60,
-					width = "auto",
-					height = "auto",
-				},
-			},
-		},
-	},
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+        {
+            "rcarriga/nvim-notify",
+            opts = {
+                max_width = 70,
+                render = "compact",
+                timeout = 3500,
+                top_down = false,
+            },
+        },
+    },
+    opts = {
+        views = {
+            cmdline_popup = {
+                border = {
+                    style = "rounded",
+                    padding = { 1, 1 },
+                },
+                size = {
+                    min_width = 60,
+                    width = "auto",
+                    height = "auto",
+                },
+            },
+        },
+    },
 }
