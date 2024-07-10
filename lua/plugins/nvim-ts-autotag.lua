e2a06692f2748e41cac316d45e592a74cc4029d3
Andrew Kohn <contact@kohnandrew.com>
2024-07-10 10:56:38 -0500

    Update: plugin setup change

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -2,13 +2,30 @@
 -- autoclose/autorename html tags
 
 return {
-	"windwp/nvim-ts-autotag",
-	dependencies = { "nvim-treesitter/nvim-treesitter" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			autotag = {
-				enable = true,
-			},
-		})
-	end,
+    "windwp/nvim-ts-autotag",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+        -- require("nvim-treesitter.configs").setup({
+        --     autotag = {
+        --         enable = true,
+        --     },
+        -- })
+
+        require("nvim-ts-autotag").setup({
+            opts = {
+                -- Defaults
+                enable_close = true, -- Auto close tags
+                enable_rename = true, -- Auto rename pairs of tags
+                enable_close_on_slash = false, -- Auto close on trailing </
+            },
+            -- Also override individual filetype configs, these take priority.
+            -- Empty by default, useful if one of the "opts" global settings
+            -- doesn't work well in a specific filetype
+            per_filetype = {
+                ["html"] = {
+                    enable_close = false,
+                },
+            },
+        })
+    end,
 }
